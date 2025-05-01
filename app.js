7e0de57c0a0fbf6f5b534396bd9077f6087dd705
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2025-05-01 22:54:06 +0700

    Permission Geolocation

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+function success(position) {
+    console.log(position);
+}
+
+function error(position) {
+    alert('Posis tidak dapat di akses !!!');
+}
+
+function userLocation() {
+    if(!navigator.geolocation) {
+        alert('Geolocation is not supported!');
+    }else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
 function index(){
     let app         = document.getElementById('app');
     let h3          = document.createElement('h3');
     h3.innerHTML    = 'Prayer Times';
 
     app.appendChild(h3)
+
+    userLocation()
 }
 
 index();
